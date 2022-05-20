4cd5ad63574514b9710c90d033a1c64bb0f835ab
Nsikak Imoh <drmacsika@gmail.com>
2022-05-20 02:04:49 +0100

    Add minor enhancement for code readability

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,6 +1,19 @@
-const context = ({ req }: {req: any}) => {  
+/**
+ * 
+ * The context function is part of the ApolloServer constructor.
+ * Provides configuration attributions that are accessible when the 
+ * server runs.
+ * 
+ * @param {request}
+ * @returns apiKey: string, userAgent: string
+ */
+
+require('dotenv').config()
+
+const context = (req?: {req: any}): {apiKey: string, userAgent: string} => {  
+  const apiKey = process.env.PANDASCORE_API_KEY
     return {
-      apiKey: "8sCOL40JsUIUb5haQHaNFUrX-C3CqyLGnt8-u4KZby4OU8EvhO4",
+      apiKey: String(apiKey),
       userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
     }
   }
